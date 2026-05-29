59314e0adbc1d45971cbb95036904f60b97a7552
Jin619 <migueld.ruizs@konradlorenz.edu.co>
2026-05-28 19:16:41 -0500

    hotfix merge

--- src/components/SellerOnboardingPanel.jsx
+++ src/components/SellerOnboardingPanel.jsx
@@ -8,12 +8,8 @@ import {
   renovarSuscripcion,
 } from '../services/sellerService.js';
 import { refreshSession, sincronizarVendedorDesdeSolicitud } from '../services/authService.js';
-<<<<<<< HEAD
 import { registrarSolicitudAprobada } from '../services/analyticsService.js';
-import { FORMATOS_LEGALES_VENDEDOR } from '../data/marketplaceContent.js';
-=======
 import { FORMATOS_LEGALES_VENDEDOR, SITE_NAME } from '../data/marketplaceContent.js';
->>>>>>> cb9a7ca1511811375ea3ea83d43f6ce1734eb16c
 import { createProducto } from '../services/productService.js';
 import { findUsuarioByIdentity } from '../services/userService.js';
 import { buildCategoriasFromSelection } from '../data/productCategories.js';
